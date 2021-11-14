b545b859a363c1658cad780fd72d1672f7be95c9
Bhola-kr <bholakumar2298@gmail.com>
2021-11-14 11:46:38 +0530

    First commit

---
+++ app/src/main/java/com/videocall/avideochat/screens/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.videocall.avideochat.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.videocall.avideochat.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_splash );
+    }
+}
